7b386b5ceb500e17c9b31614c40fe5f8519bc41d
Charu Thomas <charu@getox.com>
2023-11-29 17:32:03 -0600

    added markers in plot for agent start and ttreasure

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import matplotlib.pyplot as plt
 
 def initializeGame():
 
-	randInt = np.random.randint(0,4, size=4)
+	randInt = np.random.randint(1,5, size=4)
 
-	treasure_coordinates = (randInt[0], randInt[1])
-	start_coordinates = (randInt[2], randInt[3])
+	treasure_coordinates = (randInt[0] - 1, randInt[1] - 1)
+	start_coordinates = (randInt[2] - 1, randInt[3] - 1)
 
 	print(treasure_coordinates, start_coordinates)
 
 	data = np.arange(1,17).reshape((4,4))
 	fig, ax = plt.subplots()
 	cax = ax.imshow(data, cmap='viridis')
+
+	ax.scatter(start_coordinates[0], start_coordinates[1], c='black', marker='.', label='Start')
+	ax.scatter(treasure_coordinates[0], treasure_coordinates[1], c='gold', marker='X', label='Treasure')
+
 
 	ax.set_xticks(np.arange(4))
 	ax.set_yticks(np.arange(4))
