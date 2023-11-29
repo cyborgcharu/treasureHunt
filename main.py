5994b808c155178c058668ce947a65c01042ef72
Charu Thomas <charu@MacBook-Air-3.local>
2023-11-29 17:01:48 -0600

    added matplotlib grid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
 
 def initializeGame():
 	"""Generates random coordinates for treasure and agent start
@@ -19,6 +19,17 @@ def initializeGame():
 	start_coordinates = (randInt[2], randInt[3])
 
 	print(treasure_coordinates, start_coordinates)
+
+	data = np.arange(1,17).reshape((4,4))
+	fig, ax = plt.subplots()
+	cax = ax.imshow(data, cmap='viridis')
+
+	ax.set_xticks(np.arange(4))
+	ax.set_yticks(np.arange(4))
+	ax.set_xticklabels(np.arange(1, 5))
+	ax.set_yticklabels(np.arange(1, 5))
+
+	plt.show()
 
 
 def main():
