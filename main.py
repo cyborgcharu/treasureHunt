3faa7305a5a6b7766eced9b51f8620245a776815
Charu Thomas <charu@MacBook-Air-3.local>
2023-11-29 16:48:50 -0600

    added game init

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
-import args
+import numpy as np
+import matplotlib
+
+def initializeGame():
+	"""Generates random coordinates for treasure and agent start
+
+	Returns
+	-------
+	treasure_coordinates: 
+		tuple of treasure x, y location
+
+	start_coordinates:
+		tuple of agent start x, y location
+	"""
+
+	randInt = np.random.randint(0,4, size=4)
+
+	treasure_coordinates = (randInt[0], randInt[1])
+	start_coordinates = (randInt[2], randInt[3])
+
+	print(treasure_coordinates, start_coordinates)
+
+
+def main():
+	initializeGame()
 
 if __name__ == "__main__":
 	main()
